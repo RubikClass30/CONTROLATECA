ec597d256154dc3be2d77b78ae996d3885fbee17
dam <dam@203P11>
2022-02-04 11:50:03 +0100

    creada clase 2 en rama produccion

---
+++ src/refactorizar/Clase2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package refactorizar;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    
+    public Clase2(){
+        System.out.println("clase creada");
+    }
+    
+}
