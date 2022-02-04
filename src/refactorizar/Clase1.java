0385f8998ea6e1dd2fb6107ae77cd95741e4a108
dam <dam@203P11>
2022-02-04 11:39:24 +0100

    primera version

---
+++ src/refactorizar/Clase1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package refactorizar;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("ejercicio de examen");
+        System.out.println("control de versiones");
+    }
+    
+}
